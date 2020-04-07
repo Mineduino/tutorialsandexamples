85196cdc934fc448522b94806fb924c0737ff3c0
Adam Raźniewski <adam@razniewski.eu>
2020-04-07 15:22:46 +0200

    verbose

--- self-hosted-heroku/entrypoint.sh
+++ self-hosted-heroku/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo $PORT
 cat /etc/nginx/conf.d/default.conf.template | sed "s/PORTTOCHANGE/$PORT/g" > /etc/nginx/conf.d/default.conf
-nginx -c /etc/nginx/nginx.conf -g 'daemon off;'& mosquitto -c /mosquitto/config/mosquitto.conf& npm start -- --userDir /data --port 1880 --settings /settings.js
+nginx -c /etc/nginx/nginx.conf -g 'daemon off;'& mosquitto -v -c /mosquitto/config/mosquitto.conf& npm start -- --userDir /data --port 1880 --settings /settings.js
 
